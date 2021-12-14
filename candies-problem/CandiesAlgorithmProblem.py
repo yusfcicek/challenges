9d686c1272b36f28ba839d8e1259001c84924a91
Yusuf Çiçek <66864981+yusfcicek@users.noreply.github.com>
2021-12-14 14:51:34 +0300

    Update CandiesAlgorithmProblem.py

--- candies-problem/CandiesAlgorithmProblem.py
+++ candies-problem/CandiesAlgorithmProblem.py
@@ -1,35 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'candies' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
 def candies(n, arr):
-    result = 0
+    # Write your code here
+    candie_arr = [1] * n
 
-    candies_arr = [1] * n
-
-    for x in range(1, n):
-        if arr[x] > arr[x - 1]:
-            candies_arr[x] = candies_arr[x - 1] + 1
-
-    for x in range(n - 2, -1, -1):
-        if arr[x] > arr[x + 1] and candies_arr[x] <= candies_arr[x + 1]:
-            candies_arr[x] = candies_arr[x + 1] + 1
-
-    for j in candies_arr:
-        result += j
-
-    return result
+    for i in range(n-1):
+        if arr[i+1] > arr[i]:
+            candie_arr[i+1] = candie_arr[i] + 1
+            
+    for j in range(n-1, 0, -1):
+        if arr[j-1] > arr[j] and candie_arr[j-1] <= candie_arr[j]:
+            candie_arr[j-1] = candie_arr[j] + 1
+            
+    return sum(candie_arr)
 
 
-def main():
-    n = int(input())
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
 
     arr = []
 
-    for i in range(n):
-        X = int(input())
-        arr.append(X)
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
 
     result = candies(n, arr)
 
-    print(result)
+    fptr.write(str(result) + '\n')
 
-
-if __name__ == "__main__":
-    main()
+    fptr.close()
